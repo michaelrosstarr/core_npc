7f336d5e06ae7728a1854b2bc5bf1d39db3a7d9b
Michael Ross Tarr <mrmrmtarr@gmail.com>
2021-08-01 01:28:10 +0200

    Updated config design

--- config.lua
+++ config.lua
@@ -8,3 +8,17 @@ Config.Peds = {
     -- { x, y, z, ped heading, model hash, ped model, heading text, animation info }
     {-56.800365447998, -785.83428955078, 43.227298736572, 500.77, 0xC99F21C4,"a_m_y_business_01", "Business Man", "mini@strip_club@idles@bouncer@base"}
 }
+
+Config.updatedPeds = {
+    {
+        headingText = "Business Man",
+        model = "a_m_y_business_01",
+        animation = "mini@strip_club@idles@bouncer@base",
+        coords = {
+            x = 123,
+            y = 123,
+            z = 123,
+            heading = 123,
+        }
+    },
+}
